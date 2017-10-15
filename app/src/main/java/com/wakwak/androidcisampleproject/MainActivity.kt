95cbebf4833afc167f3d339a65ff2b6fc725b839
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-10-16 00:50:05 +0900

    Add Fabric

--- app/src/main/java/com/wakwak/androidcisampleproject/MainActivity.kt
+++ app/src/main/java/com/wakwak/androidcisampleproject/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.wakwak.androidcisampleproject
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Fabric.with(this, Crashlytics())
     }
 }
